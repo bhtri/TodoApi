e67924a0a8e7c90cdf09d620df2b6d405eb67226
wdvqmj7377 <16353896+bhtri@users.noreply.github.com>
2021-01-25 17:09:04 +0900

    add Entities

---
+++ TodoApi/Entities/AppConfig.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace TodoApi.Entities
+{
+    public class AppConfig
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
--- TodoApi/Entities/Product.cs
+++ TodoApi/Entities/Product.cs
@@ -1,10 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace TodoApi.Entities
 {
     public class Product
     {
-        public Product()
-        {
-        }
+        public int Id { get; set; }
+        public decimal Price { get; set; }
+        public decimal OriginalPrice { get; set; }
+        public int Stock { get; set; }
+        public int ViewCount { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string SeoAlias { get; set; }
+
+        public List<ProductInCategory> ProductInCategories { get; set; }
+
+        public List<OrderDetail> OrderDetails { get; set; }
+
+        public List<Cart> Carts { get; set; }
+
+        public List<ProductTranslation> ProductTranslations { get; set; }
     }
 }
+
+///
+/// Cau hinh thuoc tinh kieu du lieu ntn trong DB (Configure entity):
+/// +> Attribute configuration (dung cac thuoc tinh de chi ra [Table], [Required], ...)
+/// +> Fluent API configuration (* common) (khong dung gi toi class -> config o ngoai)
+///
